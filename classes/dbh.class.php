5c66065b10c02ccf48b6ad0d171a917a105d4fee
unknown <anastarak9@gmail.com>
2020-02-20 17:52:17 +0100

    updated

--- classes/dbh.class.php
+++ classes/dbh.class.php
@@ -7,10 +7,10 @@
         private $dbname;
 
         protected function connect(){
-            $this->servername="localhost";
-            $this->username="root";
-            $this->password="";
-            $this->dbname="Etudiants";
+            $this->servername="eu-cdbr-west-02.cleardb.net";
+            $this->username="be1b80205d11c8";
+            $this->password="0f65be0d";
+            $this->dbname="Etudiantss";
             $con = new mysqli($this->servername,$this->username,$this->password,$this->dbname);
             return $con;
         }
